a85e9a6622aa14a4aa5c8de132b8f4f64e94e59f
Pitigoi <lascuzeanu_stefan@yahoo.com>
2026-02-18 16:04:42 +0200

    Fix manifest icon paths

--- vite.config.ts
+++ vite.config.ts
@@ -21,17 +21,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/logo192.png",
+            src: "/panic-pwa/logo192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/logo512.png",
+            src: "/panic-pwa/logo512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/logo512.png",
+            src: "/panic-pwa/logo512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
